8e8426deca8b6ae533810e574238a4d5d2054413
jashjacob <jash1993@gmail.com>
2014-10-16 16:30:34 +0530

    image loads from the feed inside card

--- Card Based RSS Feed/feed.js
+++ Card Based RSS Feed/feed.js
@@ -8,7 +8,13 @@ google.load("feeds", "1");
       {
         result.feed.entries.forEach(function(entry)
         {
-          $('#feed').append('<div class="card"><a href="' + entry.link + '">' + entry.title + '</a><p>' + entry.contentSnippet + '</p><br><p>'+entry.publishedDate+'</div>');
+        	var images = $(entry.content).find('img').map(function(){
+                       return $(this).attr('src')
+                       }).get();
+          //var placeholder = placeholder.png;
+            $('#feed').append('<div class="card"><a href="' + entry.link + '">' + entry.title + '</a><p>' + entry.contentSnippet + '</p><br><p class="pubdate">'+ entry.publishedDate +'"</p> <br> <img src="'+(images.length == 0 ? 'placeholder.png' : images[0])+'"/></div>');
+          //$('#feed').append('<div class="card"><img src="'+ images[0] +'"/><a href="' + entry.link + '">' + entry.title + '</a><p>' + entry.contentSnippet + '</p><br><p>'+ entry.publishedDate +'"</p></div>');
+        //  $('#feed').append('<div class="card"><a href="' + entry.link + '">' + entry.title + '</a><p>' + entry.contentSnippet + '</p><br><p>'+entry.publishedDate+'</div>');
         });
       }
     });
